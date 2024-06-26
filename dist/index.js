f639849fdc7a3d375257c2c3d9bb4a1e6746010f
Himanshu V Chaurasia <himanshuchaurasia015@gmail.com>
2024-06-26 19:28:14 +0530

    done

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,57 @@
 "use strict";
 const Username = document.querySelector("#user");
-const formSubmit = document.querySelector(".form");
-const main_container = document.querySelector(".main-container");
+const formSubmit = document.querySelector("#form");
+const main_container = document.querySelector(".main_container");
 async function myCustomFetcher(url, options) {
-    const response = await fetch(url);
+    const response = await fetch(url, options);
     if (!response.ok) {
         throw new Error(`Network response not ok- status:${response.status}`);
     }
     const data = await response.json();
-    console.log(data);
+    //  console.log(data)
     return data;
 }
 const showResult = (user) => {
+    const { avatar_url, login, url } = user;
+    const element = `<div class='card'>
+        <hr/>
+        <div class='card'>
+        <img src=${avatar_url} alt=${login}/>
+        <p style="color:white"><u>${login}</u></p>
+        <a href="${url}"> Github</a>
+        </div>
+        </div>`;
+    main_container.insertAdjacentHTML("beforeend", element);
 };
-const fetchUserData = (url) => {
+function fetchUserData(url) {
     myCustomFetcher(url, {}).then((userInfo) => {
         for (const user of userInfo) {
             showResult(user);
         }
     });
-};
+}
 //default funtion call
 fetchUserData("https://api.github.com/users");
+formSubmit.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const searchTerm = Username.value.toLowerCase();
+    try {
+        const url = `https://api.github.com/users`;
+        const allUserData = await myCustomFetcher(url, {});
+        const matchingUsers = allUserData.filter((user) => {
+            return user.login.toLowerCase().includes(searchTerm);
+        });
+        main_container.innerHTML = "";
+        if (matchingUsers.length === 0) {
+            main_container.insertAdjacentHTML("beforeend", `<p class="empty-msg"> No matching users found.</p>`);
+        }
+        else {
+            for (const single of matchingUsers) {
+                showResult(single);
+            }
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 const Username =document.querySelector("#user") as HTMLInputElement;
-const formSubmit =document.querySelector(".form") as HTMLFormElement;
-const main_container =document.querySelector(".main-container") as HTMLElement;
+const formSubmit =document.querySelector("#form") as HTMLFormElement;
+const main_container =document.querySelector(".main_container") as HTMLElement;
 
 //define contract of an object
 interface UserData{
@@ -11,31 +11,79 @@ interface UserData{
     url:string;
 }
 
-async function myCustomFetcher<T>(url:string, options?:RequestInit):Promise<T>{
-     const response = await fetch(url);
+async function myCustomFetcher<T>(
+    url:string, 
+    options?:RequestInit
+    ):Promise<T>{
+     const response = await fetch(url,options);
      if (!response.ok){
-        throw new Error(`Network response not ok- status:${response.status}`);
+        throw new Error(
+            `Network response not ok- status:${response.status}`
+        );
      }
      const data= await response.json();
-     console.log(data)
+    //  console.log(data)
      return data;
 }
 
 
 const showResult=(user:UserData)=>{
+    const {avatar_url,login,url}=user;
+    const element=`<div class='card'>
+        <hr/>
+        <div class='card'>
+        <img src=${avatar_url} alt=${login}/>
+        <p style="color:white"><u>${login}</u></p>
+        <a href="${url}"> Github</a>
+        </div>
+        </div>`
+    main_container.insertAdjacentHTML("beforeend", element);
 
-}
+};
 
-const fetchUserData=(url:string)=>{
-    myCustomFetcher<UserData[]>(url,{}).then((userInfo)=>{
+function fetchUserData(url:string){
+    myCustomFetcher<UserData[]>(url,{}).then((userInfo:UserData[])=>{
         for (const user of userInfo) {
-            showResult(user);
-            
+            showResult(user);  
         }
-    })
-
+    });
 }
 
 //default funtion call
 fetchUserData("https://api.github.com/users")
+
+formSubmit.addEventListener("submit",async(e)=>{
+    e.preventDefault();
+    const searchTerm= Username.value.toLowerCase();
+    try {
+        const url  =`https://api.github.com/users`;
+        const allUserData:UserData[]=await myCustomFetcher(url,{});
+        const matchingUsers:UserData[]=allUserData.filter((user:UserData):boolean=>{
+            return user.login.toLowerCase().includes(searchTerm)
+
+        });
+        main_container.innerHTML="";
+        
+
+        if (matchingUsers.length===0) {
+
+            main_container.insertAdjacentHTML(
+                "beforeend",
+                `<p class="empty-msg"> No matching users found.</p>`
+            )
+            
+        }else{
+            for(const single of matchingUsers){
+
+                showResult(single);
+
+            }
+        }
+        
+    } catch (error) {
+    console.log(error);
+
+        
+    }
+})
 
